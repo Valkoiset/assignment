059be6a3db81aacc65d3626adcb718d3e1ca784b
Valkoiset <alexandrbelui2@gmail.com>
2024-02-27 00:19:27 +0100

    Added drop_columns()

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from package.functions import filter_by_country, process_data
+from package.functions import process_data
 from pyspark.sql import SparkSession
 
 
@@ -8,10 +8,9 @@ def main(dataset_one_path: str, dataset_two_path, countries):
     dataset_one = spark.read.csv(dataset_one_path, header=True)
     dataset_two = spark.read.csv(dataset_two_path, header=True)
 
-    # processed_df = process_data(dataset_one, dataset_two, countries)
-    dataset_one_filtered = filter_by_country(dataset_one, countries)
-    dataset_one_filtered.show()
-    
+    processed_df = process_data(dataset_one, dataset_two, countries)
+    processed_df.show()
+
     # processed_df.write.csv('client_data/result.csv', header=True)
 
 
@@ -21,11 +20,3 @@ if __name__ == '__main__':
         dataset_two_path='../input_data/dataset_one.csv',
         countries=['United Kingdom', 'Netherlands']
     )
-
-    # spark = SparkSession.builder.appName('KommatiPara').getOrCreate()
-    #
-    # dataset_one = spark.read.csv('../input_data/dataset_one.csv', header=True)
-    # dataset_two = spark.read.csv('../input_data/dataset_one.csv', header=True)
-    #
-    # df = filter_by_country(dataset_one, ['United Kingdom', 'Netherlands'])
-    # df.show()
--- app/package/functions.py
+++ app/package/functions.py
@@ -2,8 +2,21 @@ from pyspark.sql import DataFrame
 
 
 def process_data(dataset_one, dataset_two, countries):
-    # Implement the filtering, joining, and column renaming logic here
-    pass
+    dataset_one = drop_columns(dataset_one, ['first_name', 'last_name'])
+    dataset_one = filter_by_country(dataset_one, ['United Kingdom', 'Netherlands'])
+    return dataset_one
+
+
+def drop_columns(df: DataFrame, columns_to_remove: list) -> DataFrame:
+    """
+    Removes personal identifiable information from the dataset.
+
+    :param df: Input DataFrame containing client information.
+    :param columns_to_remove: list of columns to drop from the DataFrame.
+    :return: DataFrame with personal information columns removed.
+    """
+    df_cleaned = df.drop(*columns_to_remove)
+    return df_cleaned
 
 
 def filter_by_country(df: DataFrame, countries: list) -> DataFrame:
