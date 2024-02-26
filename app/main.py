c42db4c8c1983f1d9b0c5ad5b7993ff91bc9058a
Valkoiset <alexandrbelui2@gmail.com>
2024-02-26 23:16:26 +0100

    Added toml file

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from pyspark.sql import SparkSession
 if __name__ == '__main__':
     spark = SparkSession.builder.master('local').appName('assignment').getOrCreate()
 
-    dataset_one = spark.read.csv('../input_data/dataset_one.csv')
-    dataset_two = spark.read.csv('../input_data/dataset_one.csv')
+    dataset_one = spark.read.csv('../input_data/dataset_one.csv', header=True)
+    dataset_two = spark.read.csv('../input_data/dataset_one.csv', header=True)
 
     dataset_one.show()
