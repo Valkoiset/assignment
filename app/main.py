a8089af53401cfc79ed8777b618131cc91c988f5
Valkoiset <alexandrbelui2@gmail.com>
2024-02-26 23:50:01 +0100

    Added filter function

--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
+from package.functions import filter_by_country, process_data
 from pyspark.sql import SparkSession
 
+
+def main(dataset_one_path: str, dataset_two_path, countries):
+    spark = SparkSession.builder.appName('KommatiPara').getOrCreate()
+
+    dataset_one = spark.read.csv(dataset_one_path, header=True)
+    dataset_two = spark.read.csv(dataset_two_path, header=True)
+
+    # processed_df = process_data(dataset_one, dataset_two, countries)
+    dataset_one_filtered = filter_by_country(dataset_one, countries)
+    dataset_one_filtered.show()
+    
+    # processed_df.write.csv('client_data/result.csv', header=True)
+
+
 if __name__ == '__main__':
-    spark = SparkSession.builder.master('local').appName('assignment').getOrCreate()
+    main(
+        dataset_one_path='../input_data/dataset_one.csv',
+        dataset_two_path='../input_data/dataset_one.csv',
+        countries=['United Kingdom', 'Netherlands']
+    )
 
-    dataset_one = spark.read.csv('../input_data/dataset_one.csv', header=True)
-    dataset_two = spark.read.csv('../input_data/dataset_one.csv', header=True)
-
-    dataset_one.show()
+    # spark = SparkSession.builder.appName('KommatiPara').getOrCreate()
+    #
+    # dataset_one = spark.read.csv('../input_data/dataset_one.csv', header=True)
+    # dataset_two = spark.read.csv('../input_data/dataset_one.csv', header=True)
+    #
+    # df = filter_by_country(dataset_one, ['United Kingdom', 'Netherlands'])
+    # df.show()
--- app/package/functions.py
+++ app/package/functions.py
@@ -1,1 +1,10 @@
-#
+from pyspark.sql import DataFrame
+
+
+def process_data(dataset_one, dataset_two, countries):
+    # Implement the filtering, joining, and column renaming logic here
+    pass
+
+
+def filter_by_country(df: DataFrame, countries: list) -> DataFrame:
+    return df.filter(df.country.isin(countries))
