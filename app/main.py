f4005a94de328ffeaeed0f61dc7140d44134e018
Valkoiset <alexandrbelui2@gmail.com>
2024-02-27 17:58:23 +0100

    Added set_logging()

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
-from package.functions import process_data
+from package.logging import setup_logging
+from package.cleaning import process_data
 from pyspark.sql import SparkSession
 
 
 def main(dataset_one_path: str, dataset_two_path: str, countries: list) -> None:
+    logger = setup_logging()
+
     spark = SparkSession.builder.appName('KommatiPara').getOrCreate()
+    sc = spark.sparkContext
+
+    logger.info('SparkSession created')
 
     dataset_one = spark.read.csv(dataset_one_path, header=True)
     dataset_two = spark.read.csv(dataset_two_path, header=True)
@@ -11,7 +17,7 @@ def main(dataset_one_path: str, dataset_two_path: str, countries: list) -> None:
     df_merged = process_data(dataset_one, dataset_two, countries)
     df_merged.show()
 
-    # processed_df.coalesce(1).write.csv('../client_data/result', header=True)
+    # df_merged.coalesce(1).write.csv('../client_data/result', header=True)
 
 
 if __name__ == '__main__':
---
+++ app/package/logging.py
@@ -0,0 +1,20 @@
+from logging.handlers import RotatingFileHandler
+import logging
+
+
+def setup_logging():
+    logger = logging.getLogger('KommatiPara')
+    logger.setLevel(logging.INFO)
+
+    # Define a rotating file handler
+    handler = RotatingFileHandler(
+        './logs/my_app.log',
+        maxBytes=10 * 1024 * 1024,  # Log file size limit (10 MB)
+        backupCount=10,  # Number of backup files to keep
+    )
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
