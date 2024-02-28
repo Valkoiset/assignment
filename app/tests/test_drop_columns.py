a5382caf083f8574c9feb7f8fa36d585621b1bef
Valkoiset <alexandrbelui2@gmail.com>
2024-02-28 20:35:32 +0100

    Adjusted ci-pipeline

--- app/tests/test_drop_columns.py
+++ app/tests/test_drop_columns.py
@@ -1,5 +1,6 @@
 from chispa.dataframe_comparer import assert_df_equality
-from kommati_para.cleaning import drop_columns
+# from kommati_para.cleaning import drop_columns
+from package.cleaning import drop_columns
 from pyspark.sql import SparkSession
 import pytest
 
