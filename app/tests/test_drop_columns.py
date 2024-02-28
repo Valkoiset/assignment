99dff2e48b38a5bdce3f115614d1dc2c326f31a6
Valkoiset <alexandrbelui2@gmail.com>
2024-02-28 18:12:14 +0100

    Added 2 more tests

---
+++ app/tests/test_drop_columns.py
@@ -0,0 +1,25 @@
+from chispa.dataframe_comparer import assert_df_equality
+from kommati_para.cleaning import drop_columns
+from pyspark.sql import SparkSession
+import pytest
+
+
+@pytest.fixture(scope='session')
+def spark():
+    return SparkSession.builder.appName('TestDropColumns').getOrCreate()
+
+
+def test_drop_columns(spark):
+    data = [
+        ('Anthony Kiedis', 61, 'Michigan'),
+        ('John Frusciante', 53, 'New York')
+    ]
+    df = spark.createDataFrame(data, ['name', 'age', 'state'])
+
+    columns_to_remove = ['age', 'state']
+    cleaned_df = drop_columns(df, columns_to_remove)
+
+    expected_data = [('Anthony Kiedis',), ('John Frusciante',)]
+    expected_df = spark.createDataFrame(expected_data, ['name'])
+
+    assert_df_equality(cleaned_df, expected_df, ignore_nullable=True)
---
+++ app/tests/test_filter_by_country.py
@@ -0,0 +1,32 @@
+from chispa.dataframe_comparer import assert_df_equality
+from kommati_para.cleaning import filter_by_country
+from pyspark.sql import SparkSession
+import pytest
+
+
+@pytest.fixture(scope='session')
+def spark():
+    return SparkSession.builder.appName('TestFilterByCountry').getOrCreate()
+
+
+def test_filter_by_country(spark):
+    data = [
+        ('Anthony Kiedis', 'USA'),
+        ('John Frusciante', 'USA'),
+        ('Freddie Mercury', 'United Kingdom'),
+        ('Eva Simons', 'Netherlands'),
+        ('Alex Turner', 'United Kingdom')
+    ]
+    df = spark.createDataFrame(data, ['name', 'country'])
+
+    countries = ['United Kingdom', 'Netherlands']
+    filtered_df = filter_by_country(df, countries)
+
+    expected_data = [
+        ('Freddie Mercury', 'United Kingdom'),
+        ('Eva Simons', 'Netherlands'),
+        ('Alex Turner', 'United Kingdom')
+    ]
+    expected_df = spark.createDataFrame(expected_data, ['name', 'country'])
+
+    assert_df_equality(filtered_df, expected_df, ignore_nullable=True)
