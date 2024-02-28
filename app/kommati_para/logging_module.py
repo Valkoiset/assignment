3c168391aaf45782fb8cea97241f05220440e54f
Valkoiset <alexandrbelui2@gmail.com>
2024-02-28 20:47:12 +0100

    Adjusted ci-pipeline

--- app/kommati_para/logging_module.py
+++ app/kommati_para/logging_module.py
@@ -1,5 +1,6 @@
 from logging.handlers import RotatingFileHandler
 import logging
+import os
 
 
 def setup_logging():
@@ -8,7 +9,7 @@ def setup_logging():
 
     # Define a rotating file handler
     handler = RotatingFileHandler(
-        'logs/app.log',
+        f'{os.getcwd()}/app/logs/app.log',
         maxBytes=10 * 1024 * 1024,  # Log file size limit (10 MB)
         backupCount=10,  # Number of backup files to keep
     )
